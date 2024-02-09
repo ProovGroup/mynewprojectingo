3c9233d2c6603a099c65d8ac50989547079b41e4
RProov <139115350+RProov@users.noreply.github.com>
2024-02-09 23:33:16 +0100

    fast commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region  = "eu-west-3"
   profile = "terraform"
   assume_role {
-    role_arn = "arn:aws:iam::442809218657:role/terraform-deploy"
+    role_arn = "arn:aws:iam::335474873628:role/terraform-deploy"
   }
 }
